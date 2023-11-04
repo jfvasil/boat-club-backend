567091308ec27e58696645e2943f2a10b12a41f5
Johnny Vasil <jfvasil@gmail.com>
2023-11-04 17:30:33 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -37,10 +37,13 @@ app.use('/api/news', newsRouter)
 
 
 
-
-
-
-
-app.listen(3000, () => {
-    console.log('Server Running on Port 3000')
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
 })
+
+
+
